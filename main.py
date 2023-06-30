465d1663c29f0bd701342e8985f3f7ffd96da31c
damianokpala <110637817+damianokpala@users.noreply.github.com>
2023-06-30 19:10:41 +0000

    Drawing a car

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from turtle import Turtle, Screen
 turtle = Turtle()
-print(turtle.forward(75))
-print(turtle.backward(30))
 my_screen = Screen()
+
 
 print(my_screen.canvheight)
 print(my_screen.canvwidth)
+
+
+turtle.shape("turtle")
+turtle.forward(30)
+turtle.left(60)
+turtle.forward(30)
+turtle.right(60)
+turtle.forward(60)
+turtle.right(60)
+turtle.forward(30)
+turtle.left(60)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(150)
+turtle.right(90)
+#turtle.forward(30)
+print(turtle.position(), turtle.heading())
+
+
+
+print(turtle)
