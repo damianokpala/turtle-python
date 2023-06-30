8af197c99acdd35678fa3b2c48cca7ed8f410ee0
damianokpala <110637817+damianokpala@users.noreply.github.com>
2023-06-30 13:50:51 +0000

    in-built packages test like turtle and screen

---
+++ main.py
@@ -0,0 +1,8 @@
+from turtle import Turtle, Screen
+turtle = Turtle()
+print(turtle.forward(75))
+print(turtle.backward(30))
+my_screen = Screen()
+
+print(my_screen.canvheight)
+print(my_screen.canvwidth)
